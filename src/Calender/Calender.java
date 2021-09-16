6918d5ed058ffa0a31daca53540fc8c43fc1dfb0
jihaejae <wlgp8144@gmail.com>
2021-09-16 12:25:19 +0900

    test 또 합니다

--- src/Calender/Calender.java
+++ src/Calender/Calender.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Calender {
 
+	private static final int[] Maxdays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int maxDaysOfMonth(int month){
+		return Maxdays [month -1];
+		
+	}
 	public static void main(String[] args) {
 		System.out.println(" 일 월  화  수 목  금  토");
 		System.out.println("---------------------");
@@ -13,12 +19,11 @@ public class Calender {
 		System.out.println("22 23 24 25 26 27 28");
 		
 		Scanner scanner = new Scanner(System.in);
+		Calender cal= new Calender();
 		System.out.println("달을 입력하세요.");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.maxDaysOfMonth(month));
 		scanner.close();
 	}
 
