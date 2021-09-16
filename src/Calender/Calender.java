7294ccdd22dc73e0dc0db93a241d16bfff998502
jihaejae <wlgp8144@gmail.com>
2021-09-16 12:38:27 +0900

    test 합니다ㅋㅋㅋ

--- src/Calender/Calender.java
+++ src/Calender/Calender.java
@@ -7,6 +7,7 @@ public class Calender {
 	private static final int[] Maxdays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
 	public int maxDaysOfMonth(int month){
+		
 		return Maxdays [month -1];
 		
 	}
@@ -20,10 +21,17 @@ public class Calender {
 		
 		Scanner scanner = new Scanner(System.in);
 		Calender cal= new Calender();
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.maxDaysOfMonth(month));
+		System.out.println("반복횟수를 입력하세요");
+		int repeat = scanner.nextInt();
+		
+		for (int i = 0;i < repeat; i++) {
+			System.out.println("달을 입력하세요.");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.", month, cal.maxDaysOfMonth(month));
+			
+		}
+		System.out.println("bye~");
 		scanner.close();
 	}
 
